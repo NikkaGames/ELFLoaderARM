8f2fda185ec7594245731fb7b33757615401f880
NikkaGames <nikagames111@gmail.com>
2025-04-10 02:22:53 +0400

    push latest changes

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -17,7 +17,8 @@ LOCAL_MODULE    := zygisk
 LOCAL_CFLAGS := -w -s -Wno-error=format-security -fvisibility=hidden -fpermissive -fexceptions
 LOCAL_CPPFLAGS := -w -s -Wno-error=format-security -fvisibility=hidden -Werror -std=c++17
 LOCAL_CPPFLAGS += -Wno-error=c++11-narrowing -fpermissive -Wall -fexceptions
-LOCAL_CFLAGS += -mllvm -fla -mllvm -split -mllvm -split_num=3 -mllvm -sub -mllvm -sub_loop=2 -mllvm -bcf -mllvm -bcf_loop=2 -mllvm -bcf_prob=85 -mllvm -sobf
+#LOCAL_CFLAGS += -mllvm -fla -mllvm -split -mllvm -split_num=3 -mllvm -sub -mllvm -sub_loop=2 -mllvm -bcf -mllvm -bcf_loop=2 -mllvm -bcf_prob=85 -mllvm -sobf
+LOCAL_CFLAGS += -mllvm -split_num=1 -mllvm -sub_loop=1 -mllvm -bcf_prob=1 -mllvm -sobf
 LOCAL_LDFLAGS += -Wl,--gc-sections,--strip-all,-llog
 LOCAL_LDLIBS := -llog -landroid -lEGL -lGLESv2 -lGLESv3 -lz
 LOCAL_ARM_NEON := true
